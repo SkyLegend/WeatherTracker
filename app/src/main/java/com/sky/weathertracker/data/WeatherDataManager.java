8d6ea7de51d71badabadc6515a211fff703bc1b3
Thomas Tran <tran.thomas17@gmail.com>
2015-04-04 02:07:09 -0700

    Initial Commit
    Initial Commit

---
+++ app/src/main/java/com/sky/weathertracker/data/WeatherDataManager.java
@@ -0,0 +1,13 @@
+package com.sky.weathertracker.data;
+
+/**
+ * Determines if the data stored in SQLLite DB is still relevant based on the last time it was last
+ * update. If the data stored in SQLLite DB requires update it will invoke a call to the Weather
+ * API.
+ *
+ * @author Thomas Tran
+ * @version 1.0
+ * @since 03-29-2015
+ */
+public class WeatherDataManager {
+}
---
+++ app/src/main/java/com/sky/weathertracker/models/Weather.java
@@ -0,0 +1,7 @@
+package com.sky.weathertracker.models;
+
+/**
+ * Created by Thomas Tran on 15-03-29.
+ */
+public class Weather {
+}
